1da59dddd61533df2e052fce6881a57a1718c3cb
dkumar <deepak21188@gmail.com>
2019-09-16 16:22:05 -0400

    Basic ml.net project

---
+++ MLapp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MLappML.Model.DataModels;
+using Microsoft.ML;
+
+namespace MLapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //load teh model
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+
+            var predEngin = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            //Creating the input data
+            ModelInput input = new ModelInput();
+            while (true) { 
+            input.SentimentText = Console.ReadLine();
+
+            // Sample test- result
+            ModelOutput result = predEngin.Predict(input);
+
+            Console.WriteLine($"Text: {input.SentimentText} | Prediction: {(Convert.ToBoolean(result.Prediction) ? "Toxic" : "Non Toxic")} sentiment");
+            if (Console.Read() == 32)
+                break;
+            }
+
+
+        }
+    }
+}
